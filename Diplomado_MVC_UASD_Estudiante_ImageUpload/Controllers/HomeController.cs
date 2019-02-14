86c2dca8f319e22bea8ed7e6737f41f51be70101
wagnel <wagnelcuello@gmail.com>
2019-02-13 21:55:59 -0400

    tarea casi completa

--- Diplomado_MVC_UASD_Estudiante_ImageUpload/Controllers/HomeController.cs
+++ Diplomado_MVC_UASD_Estudiante_ImageUpload/Controllers/HomeController.cs
@@ -13,6 +13,37 @@ namespace Diplomado_MVC_UASD_Estudiante_ImageUpload.Controllers
             return View();
         }
 
+        public ActionResult FileUpload(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                EstudianteDBEntities db = new EstudianteDBEntities();
+                string ImageName = System.IO.Path.GetFileName(file.FileName);
+                string phisicalPath = Server.MapPath("~/Images/" + ImageName);
+                file.SaveAs(phisicalPath);
+                tblEstudiante estudiante = new tblEstudiante();
+                estudiante.Nombres = Request.Form["Nombres"];
+                estudiante.Apellidos = Request.Form["Apellidos"];
+                estudiante.Direccion = Request.Form["Direccion"];
+                estudiante.Telefono = Request.Form["Telefono"];
+                estudiante.Cedula = Request.Form["Cedula"];
+                estudiante.ImageUrl = ImageName;
+                db.tblEstudiante.Add(estudiante);
+                db.SaveChanges();
+            }
+            return RedirectToAction("../Home/Detalle/");
+        }
+
+        public ActionResult Detalle()
+        {
+            return View();
+        }
+
+        public ActionResult Estudiantes()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
